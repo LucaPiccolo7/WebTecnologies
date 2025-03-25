241fbb511d09d9a351ccee7c12ef3e721a1407c8
LucaPiccolo7 <luca.piccolo0705@gmail.com>
2025-03-25 20:24:52 +0100

    aggiunte

--- Assignment4/exercise.js
+++ Assignment4/exercise.js
@@ -1,9 +1,23 @@
 /*exercises*/
 "use script"
 
+/*1*/
 let studentName = "Luca";
 let age = 23;
 let isEnrolled = true;
 console.log(studentName);
 console.log(age);
 console.log(isEnrolled);
+function studentInfo(studentName, age, isEnrolled, course="Web Tecnologies"){
+    if (isEnrolled)
+        console.log(`${studentName} (${age}) is enrolled in the ${course} course`);
+    else
+        console.log(`${studentName} (${age}) is NOT enrolled in the ${course} course`);
+}
+studentInfo(studentName, age);
+studentInfo(studentName, age, isEnrolled, "INGSW");
+
+/*2*/
+let title = {
+    title: 
+}
