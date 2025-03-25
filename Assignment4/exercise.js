196d2b75b33a5695e01568f12b5b5e11c4b9faf4
LucaPiccolo7 <luca.piccolo0705@gmail.com>
2025-03-25 22:21:30 +0100

    iniziato assignment4

--- Assignment4/exercise.js
+++ Assignment4/exercise.js
@@ -18,6 +18,29 @@ studentInfo(studentName, age);
 studentInfo(studentName, age, isEnrolled, "INGSW");
 
 /*2*/
-let title = {
-    title: 
+let movie = {
+    title: "District 9",
+    director: {
+        firstName: "Neill",
+        lastName: "Blomkamp",
+        birthYear: "25/09/1979",
+        deathYear: undefined,
+        describe: describe
+    },
+    year: "25/09/2009",
+    "is part of a saga": false,
+    describe: describe
 }
+
+function describe(){
+    for(let key in this){
+        if (typeof this[key] === "object")
+            this[key].describe();
+        else {
+            if (!(typeof this[key] === "function"))
+                console.log(`${key}: ${this[key]}`);
+        }
+    }
+}
+
+movie.describe();
